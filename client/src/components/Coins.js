59b2d2c2a43baa28758899f12574bcad6342adbc
Thomakiin <Thomakiin@gmail.com>
2021-07-19 11:04:09 -0400

    remove prototype table

--- client/src/components/Coins.js
+++ client/src/components/Coins.js
@@ -83,31 +83,6 @@ const Coins = () => {
 
     return (
         <div>
-
-            <table>
-                <caption>Alien football stars</caption>
-                <tr>
-                    <th scope="col">Player</th>
-                    <th scope="col">Gloobles</th>
-                    <th scope="col">Za'taak</th>
-                </tr>
-                <tr>
-                    <th scope="row">TR-7</th>
-                    <td>7</td>
-                    <td>4,569</td>
-                </tr>
-                <tr>
-                    <th scope="row">Khiresh Odo</th>
-                    <td>7</td>
-                    <td>7,223</td>
-                </tr>
-                <tr>
-                    <th scope="row">Mia Oolong</th>
-                    <td>9</td>
-                    <td>6,21900000000000</td>
-                </tr>
-            </table>
-
             <h1>Top 50 coins</h1>
             <h2>Sort by:</h2>
             <button onClick={() => redisplayCoins(sortRankDescending)}> Rank Descending </button>
